b6c883caf06aaf69e9eab0700823ab73e3db2878
JacopoTognacci3J <jacopo.tognacci@studenti.isissgobetti.it>
2025-04-29 13:11:39 +0200

    Update Program.cs
    -aggiunte altre variabili essenziali

--- Program.cs
+++ Program.cs
@@ -10,10 +10,25 @@ namespace Battaglia_Navale
     {
         static void Main(string[] args)
         {
-            //dichiarazione ed inizializzazione delle variabili
+            //dichiarazione ed inizializzazione delle variabili:
+
+            //matrici del campo di gioco
             int[,] player = new int[10, 10];
+            int[,] playerHidden = new int[10, 10];
             int[,] ia = new int[10, 10];
             int[,] iaHidden = new int[10, 10];
+
+            //variabili coordinate per la selezione della casella durante il gioco
+            int riga = 0;
+            int colonna = 0;
+
+            //variabile turno - cambia a seconda di chi è il turno
+            char turn = ' ';
+
+
+
+
+
 
             //fin
             Console.ReadKey();
