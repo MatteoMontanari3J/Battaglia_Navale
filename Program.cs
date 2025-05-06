258daccededfeec389dd0897cb3bff285514fe58
JacopoTognacci3J <jacopo.tognacci@studenti.isissgobetti.it>
2025-05-06 13:32:13 +0200

    Preparativi
    Aggiunti:
    -commento con struttura del main
    -cambio della variabile della coordinata riga in char

--- Program.cs
+++ Program.cs
@@ -10,6 +10,41 @@ namespace Battaglia_Navale
     {
         static void Main(string[] args)
         {
+            /*
+            ######################################################################################################### 
+            STRUTTURA DEL MAIN   #  i nomi con il - affianco sono funzioni
+            ######################################################################################################### 
+            ripetizione
+            (
+                (-selezione difficoltà
+                 -assegnamento barche giocatore
+                 -assegnamento barche IA
+                 azzeramento booleano che determina l'inizio partita)
+                
+                -turno giocatore
+                    prendere coordinate
+                    -funzione sparare il colpo e vedere se si ha beccato o mancato
+
+                -controllo fine partita
+                    si: -finepartita
+                    no:(
+                        a seconda della difficoltà selezionata
+                            facile
+                                -turno IA random
+                                    generare coordinate random
+                            difficile
+                                -turno IA smart
+                                    [algoritmo intelligenza] ottenere coordinate
+
+                        -funzione sparare il colpo e vedere se si ha beccato o mancato
+
+                        -controllo fine partita
+                            si: -finepartita
+                            no: ricomincia il turno
+            )
+            #########################################################################################################                        
+            */
+
             //dichiarazione ed inizializzazione delle variabili:
 
             //matrici del campo di gioco
@@ -18,15 +53,17 @@ namespace Battaglia_Navale
             int[,] ia = new int[10, 10];
             int[,] iaHidden = new int[10, 10];
 
+            
+
             //variabili coordinate per la selezione della casella durante il gioco
-            int riga = 0;
+            char riga = ' ';
             int colonna = 0;
 
             //variabile turno - cambia a seconda di chi è il turno
             char turn = ' ';
 
 
-
+            
 
 
 
